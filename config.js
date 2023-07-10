832b7d7af1c8876b8363ca874265730c3473cde8
nemitha0 <136306345+nemitha0@users.noreply.github.com>
2023-07-10 10:03:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['0763357729', 'Nemitha', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
